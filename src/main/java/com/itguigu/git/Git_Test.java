790760df1ac374946c0e949cfa07ca00e0f40a2c
xiaotuanzi <3422764990@qq.com>
2021-10-19 16:50:59 +0800

    second commit

--- src/main/java/com/itguigu/git/Git_Test.java
+++ src/main/java/com/itguigu/git/Git_Test.java
@@ -3,5 +3,6 @@ package com.itguigu.git;
 public class Git_Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
     }
 }
